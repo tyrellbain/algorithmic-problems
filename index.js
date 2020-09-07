e3e775036a56d2c430d26553c2c2a53ee1990252
tyrellbain <contact@tyrellbain.com>
2020-09-07 19:24:56 -0400

    Add precommit

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const chalk = require("chalk");
 const question = process.argv.slice(2)[0];
 const NS_PER_MS = 1e6;
 
-fs.opendir(`./questions/`, {}, async (err, dir) => {
+fs.opendir(`./problems/`, {}, async (err, dir) => {
   let questionExists = false;
   for await (const dirent of dir) {
     if (dirent.name.split("-")[0] === question) {
       questionExists = true;
       const start = process.hrtime.bigint();
-      exec(`node ./questions/${dirent.name}/index.js`);
+      exec(`node ./problems/${dirent.name}/index.js`);
       const end = process.hrtime.bigint();
       const diff = end - start;
       console.log(
@@ -23,7 +23,7 @@ fs.opendir(`./questions/`, {}, async (err, dir) => {
         }ms to run`
       );
       console.log(chalk.cyan.underline("\nTest Results"));
-      exec(`mocha questions/${dirent.name}/test.js`, (err, stdout, stderr) => {
+      exec(`mocha problems/${dirent.name}/test.js`, (err, stdout, stderr) => {
         console.log(stdout);
       });
     }
--- scripts/read-me-generator.js
+++ scripts/read-me-generator.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.opendir(`./questions/`, {}, async (err, dir) => {
+fs.opendir(`./problems/`, {}, async (err, dir) => {
   let content =
     "# Algorithmic Problems\nA collection of algorithmic problems from leetcode\n## Quickstart\n1. Clone repo\n```sh\n$ git clone git@github.com:tyrellbain/algorithmic-problems.git\n```\n2. Install dependencies\n```sh\n$ npm install\n```\n3. Run algorithmic problem\nSelect problem # from the table of below\n```sh\n$ git run start PROBLEM-NUMBER\n";
   const regex = /^(\..+)/;
