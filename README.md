7b68fdaa8d8d8b617bdcd9943bf572c76a072790
tyrellbain <contact@tyrellbain.com>
2020-09-07 19:37:50 -0400

    Update read me generator

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# leetcode
-A collection of leetcode questions
-
- ## 38
- ## 1544
+# Algorithmic Problems
+A collection of algorithmic problems from leetcode
+## Quickstart
+1. Clone repo
+```sh
+$ git clone git@github.com:tyrellbain/algorithmic-problems.git
+```
+2. Install dependencies
+```sh
+$ npm install
+```
+3. Run algorithmic problem
+Select problem # from the table of below
+```sh
+$ git run start PROBLEM-NUMBER
+```
+## Table of Contents
+ ## 38. Count And Say
+ ## 1544. Make The String Great
--- scripts/read-me-generator.js
+++ scripts/read-me-generator.js
@@ -2,13 +2,20 @@ const fs = require("fs");
 
 fs.opendir(`./problems/`, {}, async (err, dir) => {
   let content =
-    "# Algorithmic Problems\nA collection of algorithmic problems from leetcode\n## Quickstart\n1. Clone repo\n```sh\n$ git clone git@github.com:tyrellbain/algorithmic-problems.git\n```\n2. Install dependencies\n```sh\n$ npm install\n```\n3. Run algorithmic problem\nSelect problem # from the table of below\n```sh\n$ git run start PROBLEM-NUMBER\n";
+    "# Algorithmic Problems\nA collection of algorithmic problems from leetcode\n## Quickstart\n1. Clone repo\n```sh\n$ git clone git@github.com:tyrellbain/algorithmic-problems.git\n```\n2. Install dependencies\n```sh\n$ npm install\n```\n3. Run algorithmic problem\nSelect problem # from the table of below\n```sh\n$ git run start PROBLEM-NUMBER\n```\n## Table of Contents";
   const regex = /^(\..+)/;
   for await (const dirent of dir) {
-    console.log(dirent.name.match(regex) === null);
     if (dirent.name.match(regex) === null) {
-      const number = dirent.name.split("-")[0];
-      content += `\n ## ${number}`;
+      const splitName = dirent.name.split("-");
+      const number = splitName.shift();
+      const title = splitName
+        .map((word) => {
+          const wordArr = word.split("");
+          wordArr[0] = word[0].toUpperCase();
+          return wordArr.join("");
+        })
+        .join(" ");
+      content += `\n ## ${number}. ${title}`;
     }
   }
   fs.writeFileSync("README.md", content);
